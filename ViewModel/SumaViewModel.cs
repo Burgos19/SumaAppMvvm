47dcfa8ea201dd172b9e392d8df7049ebc28d7a7
Burgos19 <burgosponce2001@gmail.com>
2025-03-29 19:55:27 -0600

    Add files via upload

---
+++ ViewModel/SumaViewModel.cs
@@ -0,0 +1,44 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System.Windows.Input;
+
+namespace SumaAppMvvm.ViewModels;
+
+public partial class SumaViewModel : ObservableObject
+{
+    [ObservableProperty]
+    private string numero1;
+
+    [ObservableProperty]
+    private string numero2;
+
+    [ObservableProperty]
+    private string resultado;
+
+    [RelayCommand]
+    private void Sumar()
+    {
+        if (string.IsNullOrWhiteSpace(Numero1) || string.IsNullOrWhiteSpace(Numero2))
+        {
+            Resultado = "Debe ingresar ambos números.";
+            return;
+        }
+
+        if (double.TryParse(Numero1, out double n1) && double.TryParse(Numero2, out double n2))
+        {
+            Resultado = $"Resultado: {n1 + n2}";
+        }
+        else
+        {
+            Resultado = "Ingrese solo valores numéricos válidos.";
+        }
+    }
+
+    [RelayCommand]
+    private void Limpiar()
+    {
+        Numero1 = string.Empty;
+        Numero2 = string.Empty;
+        Resultado = string.Empty;
+    }
+}
---
+++ Views/SumaPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace SumaAppMvvm.Views;
+
+public partial class SumaPage : ContentPage
+{
+	public SumaPage()
+	{
+		InitializeComponent();
+	}
+}
